3d0926acc47a1177f2e8583634a80889cf103a62
GaganSingh11 <gsmultani11@gmail.com>
2022-05-31 11:08:11 +0200

    established 2 end points with three handlers

--- app/main.py
+++ app/main.py
@@ -1,19 +1,52 @@
+from urllib import request
 from tornado.web import Application, RequestHandler
 import tornado.ioloop
+import json
 
+# database
+db = [
+    {
+        "test_number": 1,
+        "test_string": "first test added",
+        "test_boolen": False
+    }
+]
 
-class HelloHandler(RequestHandler):
+# views
+class TestHandler(RequestHandler):
     def get(self):
-        self.write({"message" : "Hello World"})
+        self.write({"message" : db})
+
+    def post(self):
+        if self.request.body:
+            test_byt = self.request.body
+            test_dict = json.loads(test_byt.decode('utf-8'))
+
+            db.append(test_dict)
+
+            print(test_dict)
+            self.write({"message": test_dict})
+
+class SingleTestHandler(RequestHandler):
+    def get(self, id):
+        if id:
+            print(type(id))
+            for item in db:
+                if item["test_number"] == int(id):
+                    result = item
+                else:
+                    result = "Test not found"
+        print(result)
+        self.write({"message": result})
+                
 
 
-
-
-
+# server
 def make_app():
     return Application(
         [
-            (r"/", HelloHandler)
+            (r"/tests", TestHandler),
+            (r"/tests/([^/]+)?", SingleTestHandler)
         ],
         debug = True,
         autoreload = True
